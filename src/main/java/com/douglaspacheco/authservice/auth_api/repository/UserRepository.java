2e1bf2115177ad64cde23894aa71f80ba41ada03
dougpacx <douglashxhp@gmail.com>
2026-05-04 12:20:03 -0300

    feat:add UserRepository

---
+++ src/main/java/com/douglaspacheco/authservice/auth_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.douglaspacheco.authservice.auth_api.repository;
+
+import com.douglaspacheco.authservice.auth_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
