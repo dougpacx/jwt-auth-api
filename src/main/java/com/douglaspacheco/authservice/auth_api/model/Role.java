3cd5b0a05b1103abb6dd96b4168c93f732ae2ab7
dougpacx <douglashxhp@gmail.com>
2026-05-04 12:09:04 -0300

    feat: add User and Role models

---
+++ src/main/java/com/douglaspacheco/authservice/auth_api/model/Role.java
@@ -0,0 +1,8 @@
+package com.douglaspacheco.authservice.auth_api.model;
+
+import jakarta.persistence.Entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/douglaspacheco/authservice/auth_api/model/User.java
@@ -0,0 +1,30 @@
+package com.douglaspacheco.authservice.auth_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Role role;
+}
